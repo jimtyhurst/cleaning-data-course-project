03aab3c11e3ed8ef5c811a76fd8a3533377e20ee
Jim Tyhurst <jim@tyhurst.com>
2014-09-07 12:20:12 -0700

    Fixed .gitignore to handle output files.

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,9 @@
 #! /usr/bin/Rscript
+
+# Written as an exercise for Coursera's Data Science Specialization course:
+#    Getting and Cleaning Data
+#    https://class.coursera.org/getdata-007
+
 # 1. Merges the training and the test sets to create one data set.
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 # 3. Uses descriptive activity names to name the activities in the data set
@@ -6,3 +11,13 @@
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average
 #    of each variable for each activity and each subject.
 
+# Usage:
+# Run directly from the command line, which outputs on stdout:
+# $ run_analysis.R
+# -or-
+# Run with R's batch command, which outputs to file run_analysis.Rout:
+# $ R CMD BATCH run_analysis.R
+
+print("Running analysis ...", quote=FALSE)
+
+print("Completed analysis.", quote=FALSE)
